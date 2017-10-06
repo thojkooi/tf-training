185aa78912cc710082d5c9534d86027c74707822
Thomas Kooi <thomasskooi@live.nl>
2017-10-06 15:41:48 +0200

    Break main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
-}
+
 
 resource "aws_instance" "web" {
   instance_type          = "t2.micro"
